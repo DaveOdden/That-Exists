f2a3c8c22128b1b90da4e73318c4c985ca16e2f3
Dave Odden <davidpodden@gmail.com>
2013-05-21 18:00:13 -0400

    Big update to UI and functionality.
    You can now search through all your css, just your selectors or just
    attributes

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,74 @@
 $(function() {
-	//$('input textarea').click(function() { this.select(); });
-	$('#css-box').val('#id { font-size: 1.25em } \na #wrapper.mod { color: red; text-align: left } \nb section { margin: 1em }');
+	// set fade height
+	var fade_height = $('#result').height();
+	$('#results-section span').css('height',fade_height);
+		
+	// selecting input & textarea
+	$('input[type="text"], textarea').focus(function() {
+		var _self = this;
+		setTimeout(function(){ console.log(_self.select())},100);
+	});
+
+	// executing search on enter
+	$('form').keydown(function(event){    
+	    if(event.keyCode==13){ $('#search-btn').trigger('click'); return false;}
+	});
+
 	// search through css
 	$('#search-btn').click(function() {
-		// get values
-		var given_css = $('#css-box').val();
+		var str = $('#css-box').val();
 		var given_value = $('#search-box').val();
 		
-		
-		//make sure something was entered
-		if(given_value != 'Paste class, id or attribute to search for in stylesheet') {
-			// match given value with given css
-			var found = given_css.match(new RegExp(given_value));
-			
-			
-			// could not find handling
-			if(found != null) {
-				// prep to find entire selector series
-				var selector = given_css.substring(0, given_css.indexOf(given_value));
-				// searching for entire selector series
-				var neg_index = 0;
-				var stopping_power = 0;
-				
-				
-				if(selector.length > 1) {
-					while(neg_character != '}') {
-						var neg_character = selector.slice( neg_index , neg_index+1 );
-						var neg_character_index = given_css.indexOf(selector.slice( neg_index , neg_index+1 ));
-						console.log(neg_character_index +' - Negative - '+ neg_character); // '}'
-						neg_index--;
-						if(neg_character == '') { stopping_power++; }
-						if(stopping_power == 2000) { break; }
-					}
-					// locate the start position
-					var start_pos = given_css.indexOf(found) + neg_index;
-					// strip away everything before the given css
-					var the_rest = given_css.substring(given_css.indexOf(found) + neg_index); // section { } div { } 
-					// strip away everything after the given css
-					var selector_series_and_attributes = the_rest.split('}')[1] + '}'; // section { }
-					
+		// ALL
+		if($('#all-radio').is(':checked')) {
+			var tokens = str.split('}');
 
-					// other options (not used)
-					var explicit_id = given_css.substring(given_css.indexOf(given_value) - 1).split('}')[0] + ' }';
-					var attributes = given_value + ' ' + given_css.split(given_value)[1].split('}')[0] + '}';
-
-					// write to the screen
-					$('#result').html('<br><b>RESULT:</b><br><br><blockquote>' + selector_series_and_attributes + '</blockquote>');
+			$('#result').html('');
+			for (var i = 0; i < tokens.length; i++) {
+				if(tokens[i].indexOf(given_value) != -1) {
+					$('#result').html($('#result').html() + tokens[i] + '} <br>');
 				}
-				else
-					$('#result').html('Oops, something went wrong. Gotta squash this bug...');
 			}
-			else
-				$('#result').html('<br><b>RESULT:</b><br><br><blockquote>Not Found</blockquote>');
 		}
-		else
-			$('#result').html('Please input a value');
+		
+		// SELECTOR
+		if($('#selector-radio').is(':checked')) {
+			var selectors_array = new Array();
+			var selector_token = str.split('{');
+			var tokens = str.split('}');
 
+			for (var i = 0; i < selector_token.length; i++) {
+				selectors_array.push(selector_token[i].substr(selector_token[i].indexOf("}") + 1));
+			}
+			
+			$('#result').html('');
+			for (var i = 0; i < selector_token.length; i++) {
+				if(selectors_array[i].indexOf(given_value) != -1) {
+					$('#result').html($('#result').html() + tokens[i] + '} <br>');
+				}
+			}
+		}
+		
+		// ATTRIBUTE
+		if($('#attribute-radio').is(':checked')) {
+			var attribute_array = new Array();
+			var attribute_token = str.split('}');
+			var tokens = str.split('}');
+			
+			for (var i = 0; i < attribute_token.length; i++) {
+				attribute_array.push(attribute_token[i].substr(attribute_token[i].indexOf("{") + 1));
+			}
+
+			$('#result').html('');
+			for (var i = 0; i < attribute_token.length; i++) {
+				if(attribute_array[i].indexOf(given_value) != -1) {
+					$('#result').html($('#result').html() + tokens[i] + '} <br>');
+				}
+			}
+		}
+		
+		// reset fade height
+		fade_height = $('#result').height();
+		$('#results-section span').css('height',fade_height);
 	});
 });
