254ad62b57f52eb834cdae501e9876b2b61999ce
Dave Odden <davidpodden@gmail.com>
2013-06-03 22:47:31 -0400

    Major UI Improvements & Functionality Enhancements
    v1.0 needs copy and documentation in order to be called done.

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,70 @@
 $(function() {
-	// set fade height
-	var fade_height = $('#result').height();
-	$('#results-section span').css('height',fade_height);
-		
-	// selecting input & textarea
+	$('#css-box').val('body {\nmargin: 0;\nfont-family: "Helvetica Neue", Helvetica, Arial, sans-serif;\nfont-size: 13px;\nline-height: 18px;\ncolor: #333333;\nbackground-color: #ffffff;\n}\n\na {\ncolor: #0088cc;\ntext-decoration: none;\n}\n\na:hover {\ncolor: #005580;\ntext-decoration: underline;\n}\n\n');
+	
+	// initialize editor
+	var editor = CodeMirror.fromTextArea(document.getElementById("css-box"), {
+  		mode: "text/css",
+  		styleActiveLine: true,
+  		lineNumbers: true,
+  		lineWrapping: true
+	});
+	
+	// clearing white spaces from specific result spans
+	jQuery.fn.htmlClean = function() {
+	    this.contents().filter(function() {
+	        if (this.nodeType != 3) {
+	            $(this).htmlClean();
+	            return false;
+	        }
+	        else {
+	            this.textContent = $.trim(this.textContent);
+	            return !/\S/.test(this.nodeValue);
+	        }
+	    }).remove();
+	    return this;
+	}
+	
+	// handling input & textarea selection on click
 	$('input[type="text"], textarea').focus(function() {
 		var _self = this;
 		setTimeout(function(){ console.log(_self.select())},100);
 	});
 
 	// executing search on enter
-	$('#search-box').keydown(function(event){    
-	    if(event.keyCode==13){ $('#search-btn').trigger('click'); return false;}
+	$('#search-section').keydown(function(event){   
+	    if(event.keyCode==13){ $('#search-btn-two').trigger('click'); return false;}
+	});
+	
+	// search again
+	$('.back-btn').click(function() {
+		$('#results-section').removeClass('translate');
+		$('#search-box-two').focus();
 	});
 
+
 	// search through css
-	$('#search-btn').click(function() {
-		var str = $('#css-box').val();
-		var given_value = $('#search-box').val();
+	$('#search-btn-two').click(function() {
+		var str = editor.getValue();
+		var given_value = $('#search-box-two').val();
+		var pilr = new Array();
 		
 		// ALL
 		if($('#all-radio').is(':checked')) {
 			var tokens = str.split('}');
 			
-			
-			//t = t.split("\n")
-			//var tok_index = str.indexOf(tokens[3].charAt(0));
-			//alert(tok_index);
-			//var lines = str.slice(0,tok_index).split("\n");
-			//alert(str.slice(0,tok_index - tokens[2].length));
-			
-			//var lines = $("#ptest").val().split("\n");  
-			//alert(lines.length);
-			
-			
-			var ticker = 1;
-			$('#result, #lines').html('');
+			$('#result').html('');
 			for (var i = 0; i < tokens.length; i++) {
 				if(tokens[i].indexOf(given_value) != -1) {
-					//if (lines[i].Contains(someString)) {
-					//console.log(str.split(/\r?\n|\r/).length);
-					//var lines = str.split(tokens[i]);
-					//console.log(str.indexOf(tokens[i]));
-					var ini_height = $('#result').height();
 					var t = tokens[i];
 					t = str.indexOf(t);
-					var v = str.slice(0,t);
-					var lines = v.split("\n");
-					$('#lines').html($('#lines').html() + '<div>'+(lines.length+1)+'</div>');
-					$('#result').html($('#result').html() + tokens[i] + '} <br>');
-					var nudge = $('#result').height() - ini_height;
-					$('#lines div:nth-child('+ticker+')').css('marginTop',ini_height);
-					ticker++;
+	                   var v = str.slice(0,t);
+	                   var lines = v.split("\n");
+					var	line_num = lines.length+1;
+					pilr.push('<div line="'+line_num+'">' + tokens[i] + '}</div><br>');
 				}
 			}
+			$('#result').html(prettyPrintOne(pilr.join("")));
+			$('#result div span:nth-child(1)').htmlClean();
 		}
 		
 		// SELECTOR
@@ -69,9 +80,16 @@ $(function() {
 			$('#result').html('');
 			for (var i = 0; i < selector_token.length; i++) {
 				if(selectors_array[i].indexOf(given_value) != -1) {
-					$('#result').html($('#result').html() + tokens[i] + '} <br>');
+					var t = tokens[i];
+					t = str.indexOf(t);
+                    var v = str.slice(0,t);
+                    var lines = v.split("\n");
+					var	line_num = lines.length+1;
+					pilr.push('<div line="'+line_num+'">' + tokens[i] + '}</div><br>');
 				}
 			}
+			$('#result').html(prettyPrintOne(pilr.join("")));
+			$('#result div span:nth-child(1)').htmlClean();
 		}
 		
 		// ATTRIBUTE
@@ -87,13 +105,22 @@ $(function() {
 			$('#result').html('');
 			for (var i = 0; i < attribute_token.length; i++) {
 				if(attribute_array[i].indexOf(given_value) != -1) {
-					$('#result').html($('#result').html() + tokens[i] + '} <br>');
+					var t = tokens[i];
+					t = str.indexOf(t);
+                    var v = str.slice(0,t);
+                    var lines = v.split("\n");
+					var	line_num = lines.length+1;
+					pilr.push('<div line="'+line_num+'">' + tokens[i] + '}</div><br>');
 				}
 			}
+			$('#result').html(prettyPrintOne(pilr.join("")));
+			$('#result div span:nth-child(1)').htmlClean();
 		}
 		
-		// reset fade height
-		fade_height = $('#result').height();
-		$('#results-section span').css('height',fade_height);
+		$('#results-section').addClass('translate');
+		
+		if($('#result').text() == '') {
+			$('#result').html('<div id="no-content">No Results</div>');
+		}
 	});
 });
